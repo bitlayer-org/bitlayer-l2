7441dbbc9fbd3b064b28705af211d0b1f0291b32
freddie <freddi8e@gmail.com>
2024-04-02 11:47:39 +0800

    genesis hash

--- params/config.go
+++ params/config.go
@@ -30,7 +30,7 @@ var (
 	SepoliaGenesisHash = common.HexToHash("0x25a5cc106eea7138acab33231d7160d69cb777ee0c2c553fcddf5138993e6dd9")
 	GoerliGenesisHash  = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
 
-	BitlayerL2MainnetGenesisHash = common.HexToHash("0x5216bdf23b57ec0c445341e55a7538efffb81a021d0c0dbb0d8e73da205d3842")
+	BitlayerL2MainnetGenesisHash = common.HexToHash("0x4ef61fe480171b308a1a6973ede4addb4f80440d0877a8645645a6dcb0c89ea5")
 	BitlayerL2TestnetGenesisHash = common.HexToHash("0x4419058a4a569535bba3e76b73696b6438f4c5a9bf2689ae7c06f9ab9b0849b0")
 )
 
